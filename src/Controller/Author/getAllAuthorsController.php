36266833b34126fdbd6ce72afad1777cc9977c77
geo <geoboiffils34@gmail.com>
2022-11-02 18:19:29 +0100

    test: looking for api documentation

--- src/Controller/Author/getAllAuthorsController.php
+++ src/Controller/Author/getAllAuthorsController.php
@@ -3,10 +3,23 @@
 namespace App\Controller\Author;
 
 use App\Repository\AuthorRepository;
+use OpenApi\Annotations as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class getAllAuthorsController extends AbstractController
 {
-
+    /**
+     * @param AuthorRepository $authorRepository
+     * @return void
+     * @OA\Get(
+     *     path="/api/dd",
+     * )
+     */
+    public function readALlAuthors(AuthorRepository $authorRepository) : Response
+    {
+        $authors = $authorRepository->findAll();
+        return $this->json($authors);
+    }
 }
