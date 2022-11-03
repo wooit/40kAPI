f2f72fd3cc204bc24fb10556b953763e888d9673
geo <geoboiffils34@gmail.com>
2022-11-03 18:39:18 +0100

    test: looking for api documentation

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
 ];
--- src/Controller/Author/getAllAuthorsController.php
+++ src/Controller/Author/getAllAuthorsController.php
@@ -3,19 +3,24 @@
 namespace App\Controller\Author;
 
 use App\Repository\AuthorRepository;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Nelmio\ApiDocBundle\Annotation\Operation;
 use OpenApi\Annotations as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class getAllAuthorsController extends AbstractController
 {
     /**
-     * @param AuthorRepository $authorRepository
-     * @return void
-     * @OA\Get(
-     *     path="/api/dd",
+     * @Rest\Get(
+     *     path="/api/authors",
+     *     name="api_get_authors",
      * )
+     * @Operation(summary="Read all authors")
+     * @OA\Tag(name="Authors")
+     * @OA\Response(response="200", description="ok")
+     * @OA\Response(response="404", description="ko")
+     * @OA\Response(response="500", description="ko")
      */
     public function readALlAuthors(AuthorRepository $authorRepository) : Response
     {
